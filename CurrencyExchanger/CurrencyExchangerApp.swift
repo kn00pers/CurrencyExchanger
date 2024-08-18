788221b02308918ca693900dcc8e0935df189537
Goblins-Slayer <patplaypl@gmail.com>
2024-08-18 20:32:12 +0200

    Initial Commit

---
+++ CurrencyExchanger/CurrencyExchangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyExchangerApp.swift
+//  CurrencyExchanger
+//
+//  Created by Patryk Knopp on 18/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyExchangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

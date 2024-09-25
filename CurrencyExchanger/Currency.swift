3746f9f6d7c96f408f0c05bb7493898254fa447f
kn00pers <patplaypl@gmail.com>
2024-09-25 15:11:10 +0200

    0.1.1

--- CurrencyExchanger/Currency.swift
+++ CurrencyExchanger/Currency.swift
@@ -14,6 +14,32 @@ enum Currency: Double {
     case goldPiece = 1
     
     var image: ImageResource{
-        
+        switch self {
+        case .copperPenny:
+                .copperpenny
+        case .silverPenny:
+                .silverpenny
+        case .silverPiece:
+                .silverpiece
+        case .goldPenny:
+                .goldpenny
+        case .goldPiece:
+                .goldpiece
+        }
+    }
+    
+    var name: String {
+        switch self {
+        case .copperPenny:
+            "Copper Penny"
+        case .silverPenny:
+            "Silver Penny"
+        case .silverPiece:
+            "Silver Piece"
+        case .goldPenny:
+            "Gold Penny"
+        case .goldPiece:
+            "Gold Piece"
+        }
     }
 }
